4b33c596a156afc09bc868381382d31483e994d9
Cibi211 <150768302+Cibi211@users.noreply.github.com>
2024-10-28 21:00:55 +0530

    Add files via upload

---
+++ Music player/src/music.js
@@ -0,0 +1,43 @@
+let allMusic = [
+    {
+        name: "Run BTS",
+        artist: "BTS",
+        img: "run",
+        src: "run"
+    },
+
+    {
+        name: "Perfect - Divide",
+        artist: "Ed Shreeran",
+        img: "perfect",
+        src: "perfect"
+    },
+
+    {
+        name: "Infinity - Feel Something",
+        artist: "Jaymes Young",
+        img: "infinity",
+        src: "infinity"
+    },
+
+    {
+        name: "No Lie - Now Dance",
+        artist: "Sean Paul & Dua Lipa",
+        img: "nolie",
+        src: "nolie"
+    },
+
+    {
+        name: "Dandelions",
+        artist: "Ruth B",
+        img: "Dandelions",
+        src: "Dandelions"
+    },
+
+    {
+        name: "Heat Waves ",
+        artist: "Glass Animals",
+        img: "heatwaves",
+        src: "heatwaves"
+    }
+]
